a2a1692bc31f33f814b47e683a8df6e40110a156
MonaaEid <monaa_eid@hotmail.com>
2024-02-04 01:28:15 +0200

    fix delete and post states

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,12 @@
 from flask import Flask, jsonify, Blueprint
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
