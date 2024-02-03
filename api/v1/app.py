b5e5e0512fb8f9924620b8934c828c991428ccac
MonaaEid <monaa_eid@hotmail.com>
2024-02-04 01:01:13 +0200

    fix delete and post states

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,6 +27,8 @@ if __name__ == "__main__":
     #     "HBNB_API_HOST") else "0.0.0.0"
     # port = os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else 5000
     # app.run(host=host, port=port, threaded=True)
+    # app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+    #         port=int(os.getenv('HBNB_API_PORT', '5000')),
+    #         threaded=True)
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')),
-            threaded=True)
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
