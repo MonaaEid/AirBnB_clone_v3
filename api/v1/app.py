5a066ac18d016ceb6ec590852e76932887ad465f
MonaaEid <monaa_eid@hotmail.com>
2024-02-03 16:03:50 +0200

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,10 +19,10 @@ def db_teardown(self):
 
 
 if __name__ == "__main__":
-    # host = os.getenv("HBNB_API_HOST") if os.getenv(
-    #     "HBNB_API_HOST") else "0.0.0.0"
-    # port = os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else 5000
-    # app.run(host=host, port=port, threaded=True)
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', 5000)),
-            threaded=True)
+    host = os.getenv("HBNB_API_HOST") if os.getenv(
+        "HBNB_API_HOST") else "0.0.0.0"
+    port = os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else 5000
+    app.run(host=host, port=port, threaded=True)
+    # app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+    #         port=int(os.getenv('HBNB_API_PORT', 5000)),
+    #         threaded=True)
