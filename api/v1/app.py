b5180c3e3f50de81f6fab4d5bb8f80ee64f2d8c0
MonaaEid <monaa_eid@hotmail.com>
2024-02-05 23:00:40 +0200

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module start Flask application"""
 from api.v1.views import app_views
-from flask import Flask, jsonify, Blueprint
-from flask_cors import CORS
+from flask import Flask, jsonify
 from models import storage
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -12,14 +12,14 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def db_teardown():
+def db_teardown(code):
     """declare a method to handle @app.teardown_appcontext
     that calls storage.close()"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found():
+def not_found(error):
     """ handle 404 error(not found)"""
     return jsonify({"error": "Not found"}), 404
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,16 +6,6 @@ import json
 from models import storage
 
 
-classes = {
-    "amenities": "Amenity",
-    "cities": 'City',
-    "places": 'Place',
-    "reviews": 'Review',
-    "states": 'State',
-    "users": 'User'
-}
-
-
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """return a json string"""
@@ -25,6 +15,14 @@ def status():
 @app_views.route("/stats", strict_slashes=False)
 def stats():
     """retrieves the number of each objects by type"""
+    classes = {
+    "amenities": "Amenity",
+    "cities": 'City',
+    "places": 'Place',
+    "reviews": 'Review',
+    "states": 'State',
+    "users": 'User'
+    }
     stats = {}
     for key, value in classes.items():
         stats[key] = storage.count(value)
