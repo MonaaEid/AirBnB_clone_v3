e6c2dc7bd4b02356c3a9d39e53bbd355673e1f12
MonaaEid <monaa_eid@hotmail.com>
2024-02-05 19:40:01 +0200

    add user and place views

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,6 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """view for User objects that handles all default RESTFul API actions"""
-from models import storage
-from models.user import User
+
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from models import storage
+from flask import request, jsonify, abort
+from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
