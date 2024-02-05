fd3f0a9c661f9519d54155e458217d68d2f65fa2
MonaaEid <monaa_eid@hotmail.com>
2024-02-05 15:42:00 +0200

    add user and place views

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify, Blueprint
 from api.v1.views import app_views
 from models import storage
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
