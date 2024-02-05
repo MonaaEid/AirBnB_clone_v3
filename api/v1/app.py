9c34f09103ef07c6f215dfd232da08c3cf5d30c5
MonaaEid <monaa_eid@hotmail.com>
2024-02-05 20:20:49 +0200

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,10 +24,6 @@ def not_found(self):
 
 
 if __name__ == "__main__":
-    # app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-    #         port=int(os.getenv('HBNB_API_PORT', '5000')),
-    #         threaded=True)
-
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
     port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
