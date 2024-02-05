a03657f2ad3f095e88304fa9258880a77523b00a
MonaaEid <monaa_eid@hotmail.com>
2024-02-05 20:30:26 +0200

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
 from models import storage
-from os import getenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -24,6 +24,8 @@ def not_found(self):
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')),
+            threaded=True)
+    CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+    app.run()
