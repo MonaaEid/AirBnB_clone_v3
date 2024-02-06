62b857eea0d626682a1ed3d318c192ef6647d8a5
MonaaEid <monaa_eid@hotmail.com>
2024-02-06 02:32:58 +0200

    update

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,7 +87,6 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """Test that get returns specific object"""
         new_state = State(name="LA")
