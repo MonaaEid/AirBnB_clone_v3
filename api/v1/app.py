db3e5b4a0bc5de5623250c50f4b12d5cbb5f8fab
MonaaEid <monaa_eid@hotmail.com>
2024-02-06 03:19:42 +0200

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,7 @@ def not_found(error):
     """ handle 404 error(not found)"""
     return jsonify({"error": "Not found"}), 404
 
+
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')),
