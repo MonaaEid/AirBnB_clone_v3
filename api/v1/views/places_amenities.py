d04d8724549a22c53dee7f3987c2e0c5bf07b075
MonaaEid <monaa_eid@hotmail.com>
2024-02-06 02:07:48 +0200

    update

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -21,23 +21,6 @@ def get_amenities(place_id):
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                    methods=['DELETE'], strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
-    """Deletes a Amenity object"""
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    if amenity not in place.amenities:
-        abort(404)
-    place.amenities.remove(amenity)
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
                     methods=['POST'], strict_slashes=False)
 def post_amenity(place_id, amenity_id):
     """Creates a Amenity"""
